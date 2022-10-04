e474c3542f11dfe6397eb9a9962f5dad7b3e92e8
Colin Burgess <69236232+C0C0Barbet@users.noreply.github.com>
2022-10-04 05:11:17 -0500

    Commented some stuff out

--- src/App.js
+++ src/App.js
@@ -7,16 +7,18 @@ function App() {
                 <p>
                     Hello World I guess...
                 </p>
-                <iframe title="towerOfHanoi" src="
-                        https://itch.io/embed-upload/4609281?color=333333" allowFullScreen="" width="1280"
-                        height="720"><a href="https://c0c0barbet.itch.io/tower-of-hanoi-redux">Play Tower of Hanoi Redux
-                    on
-                    itch.io</a></iframe>
-                <iframe title="jailEscape" src="https://itch.io/embed-upload/4608389?color=c7c7c7" allowFullScreen=""
-                        width="1280"
-                        height="720"><a href="https://vesiiy.itch.io/prison-escape">Play Prison Escape: Freedom
-                    Awaits on
-                    itch.io</a></iframe>
+
+                {/*<iframe title="towerOfHanoi" src="*/}
+                {/*        https://itch.io/embed-upload/4609281?color=333333" allowFullScreen="" width="1280"*/}
+                {/*        height="720"><a href="https://c0c0barbet.itch.io/tower-of-hanoi-redux">Play Tower of Hanoi Redux*/}
+                {/*    on*/}
+                {/*    itch.io</a></iframe>*/}
+                {/*<iframe title="jailEscape" src="https://itch.io/embed-upload/4608389?color=c7c7c7" allowFullScreen=""*/}
+                {/*        width="1280"*/}
+                {/*        height="720"><a href="https://vesiiy.itch.io/prison-escape">Play Prison Escape: Freedom*/}
+                {/*    Awaits on*/}
+                {/*    itch.io</a></iframe>*/}
+
             </header>
         </div>
     );
