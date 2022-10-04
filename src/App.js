df5df59a176c301615a530778be6052341afba5c
Colin Burgess <69236232+C0C0Barbet@users.noreply.github.com>
2022-10-03 19:49:38 -0500

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <header className="App-header">
+                <p>
+                    Hello World I guess...
+                </p>
+                <iframe title="towerOfHanoi" src="
+                        https://itch.io/embed-upload/4609281?color=333333" allowFullScreen="" width="1280"
+                        height="720"><a href="https://c0c0barbet.itch.io/tower-of-hanoi-redux">Play Tower of Hanoi Redux
+                    on
+                    itch.io</a></iframe>
+                <iframe title="jailEscape" src="https://itch.io/embed-upload/4608389?color=c7c7c7" allowFullScreen=""
+                        width="1280"
+                        height="720"><a href="https://vesiiy.itch.io/prison-escape">Play Prison Escape: Freedom
+                    Awaits on
+                    itch.io</a></iframe>
+            </header>
+        </div>
+    );
 }
 
 export default App;
