b5335853b970ab413411acc9d37ddf7a76099b9f
Colin Burgess <69236232+C0C0Barbet@users.noreply.github.com>
2022-10-07 05:10:17 -0500

    Silly mods and undos

--- src/components/navbar/NavbarElements.js
+++ src/components/navbar/NavbarElements.js
@@ -3,8 +3,8 @@ import { NavLink as Link } from 'react-router-dom';
 import styled from 'styled-components';
 
 export const Nav = styled.nav`
-  background: rgb(0,0,0);
-  background: linear-gradient(180deg, rgba(0,0,0,1) 0%, rgba(84,131,161,0.75) 25%, rgba(84,161,135,0.5) 50%, rgba(84,131,161,0.25) 75%, rgba(0,0,0,0) 100%); 
+  background: #63D471;
+  height: 85px;
   display: flex;
   justify-content: space-between;
   padding: 0.2rem calc((100vw - 1000px) / 2);
@@ -14,7 +14,7 @@ export const Nav = styled.nav`
 `;
 
 export const NavLink = styled(Link)`
-  color: #white;
+  color: #808080;
   display: flex;
   align-items: center;
   text-decoration: none;
@@ -51,5 +51,36 @@ export const NavMenu = styled.div`
   white-space: nowrap; */
   @media screen and (max-width: 768px) {
     display: none;
+  }
+`;
+
+export const NavBtn = styled.nav`
+  display: flex;
+  align-items: center;
+  margin-right: 24px;
+  /* Third Nav */
+  /* justify-content: flex-end;
+  width: 100vw; */
+  @media screen and (max-width: 768px) {
+    display: none;
+  }
+`;
+
+export const NavBtnLink = styled(Link)`
+  border-radius: 4px;
+  background: #808080;
+  padding: 10px 22px;
+  color: #000000;
+  outline: none;
+  border: none;
+  cursor: pointer;
+  transition: all 0.2s ease-in-out;
+  text-decoration: none;
+  /* Second Nav */
+  margin-left: 24px;
+  &:hover {
+    transition: all 0.2s ease-in-out;
+    background: #fff;
+    color: #808080;
   }
 `;
