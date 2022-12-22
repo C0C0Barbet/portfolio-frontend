6e7bf1b9555706c04bbaaa00e81150e54b49be08
Colin <crb5825@gmail.com>
2022-12-22 12:34:51 -0600

    Small change to test tracking

--- src/pages/about/aboutComponents/aboutBody.js
+++ src/pages/about/aboutComponents/aboutBody.js
@@ -4,11 +4,18 @@ const AboutBody = () => {
     return (
         <div>
             <div>
-                Experienced in various languages, focused largely on Java, C#, and C/C++.
-                Accomplished at building connections in team settings for improved
-                information flow and production. Excellent communication skills combined
-                with an analytical and attentive nature. Looking forward to problem solving
-                plus developing scalable applications and software.
+                <p>
+                    Experienced in various languages, focused largely on Java, C#, and C/C++.
+                    Accomplished at building connections in team settings for improved
+                    information flow and production. Excellent communication skills combined
+                    with an analytical and attentive nature. Looking forward to problem solving
+                    plus developing scalable applications and software.
+                </p>
+            </div>
+            <div>
+                <p>
+                    Here's a small change to test!
+                </p>
             </div>
         </div>
     );
